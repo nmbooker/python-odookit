fce336d3ebeea997b971e4ea45f6ebeda280d81b
Nick Booker <nick.booker@opusvl.com>
2021-03-05 13:36:39 +0000

    Implement odookit-databases script [FAILING TESTS]

--- tests/test_database_lifecycle.py
+++ tests/test_database_lifecycle.py
@@ -21,11 +21,16 @@ def odoo_url():
     # Don't accidentally point at a real one set up in user's main environment
     return os.environ['TEST_ODOOKIT_URL']
 
+def merge_dicts(*dicts):
+    result = {}
+    for dict_ in dicts:
+        result.update(dict_)
+    return result
 
 def current_databases(url) -> frozenset:
     list_databases_result = subprocess.run(
         ['odookit-databases'],
-        env={'ODOOKIT_URL': url},
+        env=merge_dicts(os.environ, {'ODOOKIT_URL': url}),
         stdout=subprocess.PIPE,
         check=True,
     )
@@ -40,7 +45,7 @@ def test_database_lifecycle():
             '-p', 'opensesame',
             database_name(),
         ],
-        env={'ODOOKIT_URL': odoo_url()},
+        env=merge_dicts(os.environ, {'ODOOKIT_URL': odoo_url()}),
         check=True,
     )
     assert (
@@ -50,7 +55,7 @@ def test_database_lifecycle():
 
     subprocess.run(
         ['odookit-drop-database', database_name()],
-        env={'ODOOKIT_URL': odoo_url()},
+        env=merge_dicts(os.environ, {'ODOOKIT_URL': odoo_url()}),
         check=True,
     )
     assert current_databases(url=odoo_url()) == initial_databases, \
