366220d185b8ccf74e2163e2e6f3ea437bddc53e
Nick Booker <nick.booker@opusvl.com>
2021-03-08 09:57:08 +0000

    [WIP] Working out how to get a successful connection to Odoo

--- tests/test_database_lifecycle.py
+++ tests/test_database_lifecycle.py
@@ -7,7 +7,17 @@ import pytest
 @pytest.fixture()
 def postgres_container():
     create_process = subprocess.run(
-        ['docker', 'create', 'postgres:13'],
+        [
+            'docker',
+            'create',
+            'postgres:13',
+            '-e', 'POSTGRES_USER',
+            '-e', 'POSTGRES_PASSWORD',
+        ],
+        env=merge_dicts(
+            os.environ,
+            {'POSTGRES_USER': 'odoo', 'POSTGRES_PASSWORD': 'odoodbpassword'},
+        ),
         stdout=subprocess.PIPE,
         check=True,
     )
@@ -25,17 +35,42 @@ def postgres_container():
 @pytest.fixture()
 def odoo_container(postgres_container):
     creation_process = subprocess.run(
-        ['docker', 'create', 'odoo:8'],
+        [
+            'docker',
+            'create',
+            'odoo:8',
+            '--expose', '8069',
+            '-e', 'HOST',
+            '-e', 'USER',
+            '-e', 'PASSWORD',
+        ],
+        env=merge_dicts(
+            os.environ,
+            {'HOST': postgres_container, 'USER': 'odoo', 'PASSWORD': 'odoodbpassword'},
+        ),
         stdout=subprocess.PIPE,
         check=True,
     )
     odoo_container_name = creation_process.stdout.splitlines()[0]
     try:
         subprocess.run(['docker', 'start', odoo_container_name], check=True)
-        yield odoo_container_name
+        odoo_ip_address = the_only(
+            subprocess.run(
+                [
+                    'docker', 'container', 'inspect',
+                    '-f', '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}',
+                    odoo_container_name,
+                ],
+                stdout=subprocess.PIPE,
+                check=True,
+            )
+            .stdout
+            .splitlines()
+        )
+        yield odoo_ip_address.decode('utf-8')
         subprocess.run(['docker', 'stop', odoo_container_name], check=True)
     finally:
-        subprocess.run(['docker', 'rm', odoo_container_name], check=True)
+        subprocess.run(['docker', 'rm', '-f', odoo_container_name], check=True)
 
 
 def test_get_list_of_odoo_databases_exits_with_status_code_0(odoo_container):
@@ -50,6 +85,7 @@ def odoo_url(odoo_container_name):
     return f"http://{odoo_container_name}:8069"
 
 
+@pytest.mark.skip('while debugging')
 def test_database_lifecycle(odoo_container):
     initial_databases = current_databases(url=odoo_url(odoo_container))
     subprocess.run(
@@ -90,3 +126,18 @@ def merge_dicts(*dicts):
     for dict_ in dicts:
         result.update(dict_)
     return result
+
+
+def the_only(iterable):
+    iterator = iter(iterable)
+    try:
+        first = next(iterator)
+    except StopIteration:
+        raise ValueError('iterable was empty')
+    else:
+        try:
+            next(iterator)
+        except StopIteration:
+            return first
+        else:
+            raise ValueError('iterable yielded more than one value')
